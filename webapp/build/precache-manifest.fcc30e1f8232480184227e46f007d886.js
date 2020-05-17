63bb692c5c425bba0475051100dff51b31409874
Trevor Neil Loren <tloren@deakin.edu.au>
2020-05-17 17:54:20 +1000

    Reverted manifest files and added react build folder to serve as static

---
+++ webapp/build/precache-manifest.fcc30e1f8232480184227e46f007d886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5a400bb4c57f433b12628f6d0e830e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a430c3dc58230802db27",
+    "url": "/static/js/2.af8c6aa5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.af8c6aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6caeb72595c14c20d6",
+    "url": "/static/js/main.29475160.chunk.js"
+  },
+  {
+    "revision": "4d824322bb1d9b5ec98e",
+    "url": "/static/js/runtime-main.c6ec4ccb.js"
+  }
+]);
