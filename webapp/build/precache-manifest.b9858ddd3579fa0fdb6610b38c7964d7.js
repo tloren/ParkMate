aedc16173f33882030cf036e4c315f0fa79559c8
Trevor Neil Loren <tloren@deakin.edu.au>
2020-05-21 09:46:32 +1000

    Rebuilt static react web application

---
+++ webapp/build/precache-manifest.b9858ddd3579fa0fdb6610b38c7964d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745ed74e6c107603dcd246ce4b4cf28b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44df3080fe8670b6f3d",
+    "url": "/static/js/2.0e4e45bf.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/static/js/2.0e4e45bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde6495e6ce480d54f54",
+    "url": "/static/js/main.0508da3d.chunk.js"
+  },
+  {
+    "revision": "4d824322bb1d9b5ec98e",
+    "url": "/static/js/runtime-main.c6ec4ccb.js"
+  }
+]);
