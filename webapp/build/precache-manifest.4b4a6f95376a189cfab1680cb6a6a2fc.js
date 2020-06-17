40c452d4a19ee67b11187f76c49354d88d1892dc
Trevor Neil Loren <tloren@deakin.edu.au>
2020-06-17 11:16:11 +1000

    Integrated with database, added swagger UI

---
+++ webapp/build/precache-manifest.4b4a6f95376a189cfab1680cb6a6a2fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d6754bf0341b1aae19fe52e3b101b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3c531794614c18ca61",
+    "url": "/static/js/2.8e7a75d0.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/static/js/2.8e7a75d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720a782cc42413428015",
+    "url": "/static/js/main.6c129f27.chunk.js"
+  },
+  {
+    "revision": "4d824322bb1d9b5ec98e",
+    "url": "/static/js/runtime-main.c6ec4ccb.js"
+  }
+]);
